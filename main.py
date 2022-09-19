dfbfc5d4b9f35b4abbc5693fa62b7be6bfe2a8f0
Serfed <pro95319@gmail.com>
2022-09-19 22:53:33 +0500

    .

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import logging
 import os
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, executor
 from aiogram.types import Message
-from aiogram.utils.executor import start_webhook
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetHistoryRequest
 
@@ -17,24 +16,7 @@ TOKEN = os.getenv('TOKEN')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-
-# webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
-
-
-async def on_startup(_):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
-
-
-async def on_shutdown(_):
-    await bot.delete_webhook()
+logging.basicConfig(level=logging.INFO)
 
 
 async def dump_all_messages(channel):
@@ -86,13 +68,4 @@ async def delete_new_message_service(message: Message):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    executor.start_polling(dp, skip_updates=True)
